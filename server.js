b0b3a91e1fd933a0f10f7842d965a7d1f2e4d846
khchoi0301 <khchoi0301@gmail.com>
2020-04-08 15:05:27 +0900

    initial commit for graphql

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+} = require('graphql')
+
+const app = express()
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+})
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}))
+
+app.listen(5000, () => console.log('Server Running'))
