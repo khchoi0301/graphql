0e3e12fa359a4625474a10f15d6ab88f1b6e8e06
khchoi0301 <khchoi0301@gmail.com>
2020-04-08 15:47:30 +0900

    Add book field with args to Root

--- server.js
+++ server.js
@@ -47,6 +47,14 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: "Root Query",
     fields: () => ({
+        book: {
+            type: BookType,
+            description: "a single book",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parents, args) => books.find(book => book.id === args.id)
+        },
         books: {
             type: new GraphQLList(BookType),
             description: "List of books",
