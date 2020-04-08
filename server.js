ddc52b90b9fbd47dcae0254bea7acd911c9b19c4
khchoi0301 <khchoi0301@gmail.com>
2020-04-08 15:08:17 +0900

    create book query

--- server.js
+++ server.js
@@ -4,20 +4,37 @@ const {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql')
-
+const { authors, books } = require("./data")
 const app = express()
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World'
-            }
-        })
+const BookType = new GraphQLObjectType({
+    name: "book",
+    description: "This represents a book written by an author",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
     })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: "Root Query",
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: "List of books",
+            resolve: () => books
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
 })
 
 app.use('/graphql', expressGraphQL({
